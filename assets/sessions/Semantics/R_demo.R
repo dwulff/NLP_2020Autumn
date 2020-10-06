38952a9df3c7612f2615f88cd98e5cca2ac3cab3
Dirk Wulff <dirkwulff@fp-ws-b210.psycho.unibas.ch>
2020-10-06 11:43:38 +0200

    ..

--- assets/sessions/Semantics/R_demo.R
+++ assets/sessions/Semantics/R_demo.R
@@ -1,7 +1,9 @@
-# 1. Vector and list indexing
+# 1. Vector and list indexing --------------
 
-# 2. Logical statements
+# 2. Logical statements --------------
 
-# 3. for-loops
+# 3. for-loops --------------
 
-# 4. Matrix algebra (dot, outer)
+# 4. Matrix algebra (dot, outer) --------------
+
+
